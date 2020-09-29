c3895985497f175f4abdec6a9796b370d5d8bfe9
ed-zsi <58634993+ed-zsi@users.noreply.github.com>
2020-09-29 15:54:15 +0200

    AHAHAHAAAHAAAA

--- sites/script.js
+++ sites/script.js
@@ -10,9 +10,44 @@ window.onload = function () {
     });
 
     //dark theme
-    const toggle = document.getElementById("toggle");
+    //const toggle = document.getElementById("toggle");
 
-    toggle.addEventListener("change", (e) => {
-        document.body.classList.toggle("dark", e.target.checked);
+    //toggle.addEventListener("change", (e) => {
+    //    document.body.classList.toggle("dark", e.target.checked);
+    //});
+
+
+    //new and improved dark theme that i 100% did not steal
+    const switchtheme = document.querySelector(".switch-toggle");
+
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+        document.body.classList.add("dark-theme");
+    }
+
+    switchtheme.addEventListener("click", function () {
+        document.body.classList.toggle("dark-theme");
+
+        let theme = "light";
+        if (document.body.classList.contains("dark-theme")) {
+            theme = "dark";
+        }
+        localStorage.setItem("theme", theme);
     });
+
+    function save() {
+        var checkbox = document.getElementById('checkbox');
+        localStorage.setItem('checkbox', checkbox.checked);
+    }
+
+    function load() {
+        var checked = JSON.parse(localStorage.getItem('checkbox'));
+        document.getElementById("checkbox").checked = checked;
+    }
+
+    checkbox.addEventListener("click", () => {
+        save()
+    });
+
+    load();
 };
